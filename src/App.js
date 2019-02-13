b3a94b4527fb51bf0ce148374846949d1e9d0c3a
Yazan Mufleh <ymufleh@gmail.com>
2019-02-13 10:54:28 -0500

    added bootstrap and changed stylings, also created state and Contacts.jsx component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
-
+import Contacts from './components/Contacts';
+import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Header branding="Contact Manager" />
+        <div className="container">
+          <Contacts />
+        </div>
       </div>
     );
   }
