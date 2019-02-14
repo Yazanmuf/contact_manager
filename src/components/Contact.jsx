cfa7e76a284f761bc5d5168af63248f59d0ca898
Yazan Mufleh <ymufleh@gmail.com>
2019-02-13 20:29:10 -0500

    added delete pointer

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,22 +2,30 @@ import React, { Component } from 'react'
 
 class Contact extends Component {
     state = {
-        showContactInfo: true,
+        showContactInfo: false,
     }
-
-    onShowClick = (name, e) => {
-        console.log(name)
+    // Toggles on / off the button
+    onShowClick = (e) => {
+        console.log(e);
+        this.setState({ showContactInfo: !this.state.showContactInfo })
     }
     render() {
         const { contact } = this.props;
+        const { showContactInfo } = this.state
+
         return (
             <div className='card card-body mb-3'>
-                <h4> {contact.name}<button onClick={this.onShowClick.bind(this, contact.name)} className='fas fa-sort-down' /> </h4>
-                <ul className='list-group'>
-                    <li className='list-group-item'>email: {contact.email}</li>
-                    <li className='list-group-item'>number: {contact.phone}</li>
-                </ul>
-            </div>
+                <h5> {contact.name}<i onClick={this.onShowClick} className='fas fa-sort-down' style={{ cursor: 'pointer' }} />
+                    <i className="fas fa-times" style={{ cursor: 'pointer', float: 'right', color: 'red' }}></i> </h5>
+                {
+                    showContactInfo ?
+                        <ul className='list-group'>
+                            <li className='list-group-item'>email: {contact.email}</li>
+                            <li className='list-group-item'>number: {contact.number}</li>
+                        </ul> : null
+                }
+
+            </div >
         )
     }
 }
