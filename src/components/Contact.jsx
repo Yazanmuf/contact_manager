5190804f3364efb955568b27cc0f1388dc9be8fa
Yazan Mufleh <ymufleh@gmail.com>
2019-02-23 11:29:18 -0500

    working with state

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,33 +1,41 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class Contact extends Component {
-    state = {
-        showContactInfo: false,
-    }
-    // Toggles on / off the button
-    onShowClick = (e) => {
-        console.log(e);
-        this.setState({ showContactInfo: !this.state.showContactInfo })
-    }
-    render() {
-        const { contact } = this.props;
-        const { showContactInfo } = this.state
+  state = {
+    showContactInfo: false
+  };
+  // Toggles on / off the button
+  onShowClick = e => {
+    console.log(e);
+    this.setState({ showContactInfo: !this.state.showContactInfo });
+  };
+  render() {
+    const { contact } = this.props;
+    const { showContactInfo } = this.state;
 
-        return (
-            <div className='card card-body mb-3'>
-                <h5> {contact.name}<i onClick={this.onShowClick} className='fas fa-sort-down' style={{ cursor: 'pointer' }} />
-                    <i className="fas fa-times" style={{ cursor: 'pointer', float: 'right', color: 'red' }}></i> </h5>
-                {
-                    showContactInfo ?
-                        <ul className='list-group'>
-                            <li className='list-group-item'>email: {contact.email}</li>
-                            <li className='list-group-item'>number: {contact.number}</li>
-                        </ul> : null
-                }
-
-            </div >
-        )
-    }
+    return (
+      <div className="card card-body mb-3">
+        <h5>
+          {contact.name}
+          <i
+            onClick={this.onShowClick}
+            className="fas fa-sort-down"
+            style={{ cursor: 'pointer', margin: '6px' }}
+          />
+          <i
+            className="fas fa-times"
+            style={{ cursor: 'pointer', float: 'right', color: 'red' }}
+          />{' '}
+        </h5>
+        {showContactInfo ? (
+          <ul className="list-group">
+            <li className="list-group-item">email: {contact.email}</li>
+            <li className="list-group-item">number: {contact.number}</li>
+          </ul>
+        ) : null}
+      </div>
+    );
+  }
 }
 
 export default Contact;
--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -2,42 +2,42 @@ import React, { Component } from 'react';
 import Contact from './Contact';
 
 class Contacts extends Component {
-    state = {
-        contacts: [
-            {
-                id: '1',
-                name: "John Doe",
-                email: 'bro@gmail.com',
-                number: '123-123-1233',
-            },
-            {
-                id: '2',
-                name: "karinaa",
-                email: 'jkkk@gmail.com',
-                number: '123-333-1233',
-            },
-            {
-                id: '3',
-                name: "Mohinder Suresh",
-                email: 'Mosur@gmail.com',
-                number: '13123-42343-1233',
-            },
-        ]
-
-    }
-    render() {
-        const { contacts } = this.state;
-        return (
-            <React.Fragment>
-                {contacts.map(contact => (
-                    <Contact
-                        key={contact.id}
-                        contact={contact}
-                    />
-                ))}
-            </React.Fragment>
-        )
-    }
+  state = {
+    contacts: [
+      {
+        id: '1',
+        name: 'John Doe',
+        email: 'bro@gmail.com',
+        number: '123-123-1233'
+      },
+      {
+        id: '2',
+        name: 'karinaa',
+        email: 'jkkk@gmail.com',
+        number: '123-333-1233'
+      },
+      {
+        id: '3',
+        name: 'Mohinder Suresh',
+        email: 'Mosur@gmail.com',
+        number: '13123-42343-1233'
+      }
+    ]
+  };
+  render() {
+    const { contacts } = this.state;
+    return (
+      <React.Fragment>
+        {contacts.map(contact => (
+          <Contact
+            // Passing in the entire contact as a prop
+            key={contact.id}
+            contact={contact}
+          />
+        ))}
+      </React.Fragment>
+    );
+  }
 }
 
 export default Contacts;
