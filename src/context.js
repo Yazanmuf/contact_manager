b15b213f7c47f5fe087205aed17c54be41e1b587
Yazan Mufleh <ymufleh@gmail.com>
2019-02-26 15:09:23 -0500

    added dispatch and reducer to DELETE_CONTACT functionality

--- src/context.js
+++ src/context.js
@@ -3,6 +3,20 @@ import React, { Component } from 'react';
 
 const Context = React.createContext();
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'DELETE_CONTACT':
+      return {
+        ...state,
+        contacts: state.contacts.filter(
+          contact => contact.id !== action.payload
+        )
+      };
+    default:
+      return state;
+  }
+};
+
 export class Provider extends Component {
   state = {
     contacts: [
@@ -24,7 +38,9 @@ export class Provider extends Component {
         email: 'Mosur@gmail.com',
         number: '13123-42343-1233'
       }
-    ]
+    ],
+    // dispatch is part of our state, should be able to call an action from anywhere
+    dispatch: action => this.setState(state => reducer(state, action))
   };
 
   render() {
